2e0cdd9827aad0cd854fa6d5f52c7df1bd66080b
mohehexd <dumblemoe@gmail.com>
2020-11-25 15:59:12 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records){
+//   debugger
+console.log("I am here", football)
+console.log("This is records", records)
+const football = records.find(record => {return record.result === "W"} )
+return !!football ? football.year : undefined
+};
+// superbowlWin = (records) => {console.log("records")}
+// [1,3,5,6,8].find(function(e) { return e % 2 === 0 })
